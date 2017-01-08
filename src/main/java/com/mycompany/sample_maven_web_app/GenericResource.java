3928374ea4f1fc60151ad528fe9eb59fe5ef3fea
Wes Lloyd-laptop <wlloyd@acm.org>
2017-01-08 02:24:36 -0800

    new maven project

---
+++ src/main/java/com/mycompany/sample_maven_web_app/GenericResource.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.sample_maven_web_app;
+
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PUT;
+import javax.ws.rs.core.MediaType;
+import java.io.IOException;
+import javax.ws.rs.POST;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jettison.json.JSONObject;
+import objects.User;
+
+/**
+ * REST Web Service
+ *
+ * @author wlloyd
+ */
+@Path("generic")
+public class GenericResource {
+
+    @Context
+    private UriInfo context;
+
+    /**
+     * Creates a new instance of GenericResource
+     */
+    public GenericResource() {
+    }
+
+    /**
+     * Retrieves representation of an instance of services.GenericResource
+     * @return an instance of java.lang.String
+     */
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    public String getXml() {
+        //TODO return proper representation object
+        return "<html><body>Generic web service GET!</body></html>";
+    }
+
+    /**
+     * PUT method for updating or creating an instance of GenericResource
+     * @param content representation for the resource
+     */
+    @PUT
+    @Produces(MediaType.TEXT_PLAIN)
+    @Consumes(MediaType.TEXT_PLAIN)
+    public String putXml(String content) {
+        return "The user just put=" + content;
+    }
+    
+    @POST
+    @Produces(MediaType.TEXT_PLAIN)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public String PostJson(String jobj) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        //User user = mapper.readValue(jobj.toString(), User.class);
+        User user = mapper.readValue(jobj, User.class);
+        
+        StringBuilder text = new StringBuilder();
+        text.append("The JSON obj:" + jobj.toString() + "\n");
+        text.append("Hello " + user.getName() + "\n");
+        text.append("You're only " + user.getAge() + " years old.\n");
+        user.getMessages().forEach((msg) -> { text.append(msg + "\n"); });
+        
+        return text.toString();
+    }
+}
+
---
+++ src/main/java/objects/User.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package objects;
+
+import java.util.List;
+
+/**
+ *
+ * @author wlloyd
+ */
+public class User {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    private List messages;
+
+    public List getMessages() {
+        return messages;
+    }
+
+    public void setMessages(List messages) {
+        this.messages = messages;
+    }
+
+    
+}
+
+
