0b94a3c65d77319c7e22710039f66b67c24221bb
Wes Lloyd-laptop <wlloyd@acm.org>
2017-02-05 17:45:05 -0800

    updated scripts

--- src/scripts/delete_test.sh
+++ src/scripts/delete_test.sh
@@ -1,12 +1,12 @@
-#ENDPOINT=https://glacial-anchorage-84282.herokuapp.com/rest/generic
+#ENDPOINT=https://glacial-anchorage-84282.herokuapp.com/tcss360/users
 # heroku
-#ENDPOINT=https://frozen-temple-15527.herokuapp.com/rest/generic
+#ENDPOINT=https://frozen-temple-15527.herokuapp.com/tcss360/users
 # local web runner deploy
-ENDPOINT=http://localhost:8080/rest/generic
+ENDPOINT=http://localhost:8080/tcss360/users
 # net beans deploy
-#ENDPOINT=http://localhost:8084/sample_maven_web_app/rest/generic
+#ENDPOINT=http://localhost:8084/sample_maven_web_app/tcss360/users
 # manual deploy
-#ENDPOINT=http://localhost:8080/sample_maven_web_app-1.0-SNAPSHOT/rest/generic
+#ENDPOINT=http://localhost:8080/sample_maven_web_app-1.0-SNAPSHOT/tcss360/users
 curl -X DELETE -H "Content-Type: application/json" -d @./fred_delete.json $ENDPOINT
 #curl -X POST -H "Content-type: application/json" -d '{"name":"Fred Smith","age":33}' $ENDPOINT
 echo
--- src/scripts/update_test.sh
+++ src/scripts/update_test.sh
@@ -1,10 +1,10 @@
-#ENDPOINT=https://frozen-temple-15527.herokuapp.com/rest/generic
+#ENDPOINT=https://frozen-temple-15527.herokuapp.com/tcss360/users
 # local web runner deploy
-ENDPOINT=http://localhost:8080/rest/generic
+ENDPOINT=http://localhost:8080/tcss360/users
 # net beans deploy
-#ENDPOINT=http://localhost:8084/sample_maven_web_app/rest/generic
+#ENDPOINT=http://localhost:8084/sample_maven_web_app/tcss360/users
 # manual deploy
-#ENDPOINT=http://localhost:8080/sample_maven_web_app-1.0-SNAPSHOT/rest/generic
+#ENDPOINT=http://localhost:8080/sample_maven_web_app-1.0-SNAPSHOT/tcss360/users
 curl -X PUT -H "Content-Type: application/json" -d @./fred_update.json $ENDPOINT
 echo
 
