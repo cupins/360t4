d0182ef0b8c42f1a4cd215f5e6a04fc6f3c718e7
Wes Lloyd-laptop <wlloyd@acm.org>
2017-02-05 21:08:02 -0800

    db update

--- src/sql/create_database.sql
+++ src/sql/create_database.sql
@@ -15,7 +15,7 @@ create table users (
 );
 
 create table messages (
-    messageid integer,
+    messageid serial primary key,
     userid integer,
     message varchar(255),
     dateadded timestamp
