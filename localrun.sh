7cfc0d94acf058e305666757fa92d19d470913f1
Wes Lloyd-laptop <wlloyd@acm.org>
2017-02-05 17:39:22 -0800

    updated localrun script

--- localrun.sh
+++ localrun.sh
@@ -1,5 +1,21 @@
 heroku run echo $\JDBC_DATABASE_URL >.mydburl 2>.mydburlerr
 myurl=`cat .mydburl`
+if [[ -z $myurl ]]
+then
+  heroku pg:credentials > .mydburl
+  thing=`tail -n 1 .mydburl`
+  usr=`echo $thing | cut -d'/' -f 3 | cut -d':' -f 1`
+  pswd=`echo $thing | cut -d'/' -f 3 | cut -d':' -f 2 | cut -d'@' -f 1`
+  url=`echo $thing | cut -d'/' -f 3 | cut -d':' -f 2 | cut -d'@' -f 2`
+  db=`echo $thing | cut -d'/' -f 4`
+  port=`echo $thing | cut -d':' -f 4 | cut -d'/' -f 1`
+  echo "usr=$usr"
+  echo "pswd=$pswd"
+  echo "url=$url"
+  echo "db=$db"
+  echo "port=$port"
+  myurl="jdbc:postgresql://${url}:${port}/${db}?user=${usr}&password=${pswd}&sslmode=require"
+fi
 export JDBC_DATABASE_URL="$myurl"
 echo "Run application with $JDBC_DATABASE_URL DB URL."
 java -jar target/dependency/webapp-runner.jar target/sample_maven_web_app-1.0-SNAPSHOT.war
