f2a6cc56a54cd533451de00b744da956e108a2c0
cupins <thestuberg@gmail.com>
2017-02-14 20:39:09 -0800

    Added coffee shop fieldsTODO: update model and REST/json services

--- src/main/java/objects/Coffee_Shop.java
+++ src/main/java/objects/Coffee_Shop.java
@@ -10,6 +10,65 @@ package objects;
  * @author reid
  */
 public class Coffee_Shop {
+    
+    private int cid;
+    private String coffeeName;
+    private String coffeeAddress;
+    private int rawReview;
+    private String phone;
+    private String url;
+    
+    // Constructor
+    Coffee_Shop(){}
+    
+    // Getter and Setters
+    public int getCid() {
+        return cid;
+    }
+
+    public void setCid(int cid) {
+        this.cid = cid;
+    }
+
+    public String getCoffeeName() {
+        return coffeeName;
+    }
+
+    public void setCoffeeName(String coffeeName) {
+        this.coffeeName = coffeeName;
+    }
+
+    public String getCoffeeAddress() {
+        return coffeeAddress;
+    }
+
+    public void setCoffeeAddress(String coffeeAddress) {
+        this.coffeeAddress = coffeeAddress;
+    }
+
+    public int getRawReview() {
+        return rawReview;
+    }
+
+    public void setRawReview(int rawReview) {
+        this.rawReview = rawReview;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
     //Determine fields getter/setters and constructors
+    public void setUrl(String url) {
+        this.url = url;
+    }
     
 }
