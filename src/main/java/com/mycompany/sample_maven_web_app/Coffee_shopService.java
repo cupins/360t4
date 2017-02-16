99f3eceb119e7aed16f8dc0c1061be3c21895794
cupins <thestuberg@gmail.com>
2017-02-15 20:15:22 -0800

    Finished CRUD operations with coffeeshop services
    Everything is done in memory now
    # Conflicts:
    #	src/main/java/data/Model.java

--- src/main/java/com/mycompany/sample_maven_web_app/Coffee_shopService.java
+++ src/main/java/com/mycompany/sample_maven_web_app/Coffee_shopService.java
@@ -1,179 +1,381 @@
 /*
+
  * To change this license header, choose License Headers in Project Properties.
+
  * To change this template file, choose Tools | Templates
+
  * and open the template in the editor.
+
  */
+
 package com.mycompany.sample_maven_web_app;
 
+
+
 import data.Model;
+
 import java.io.IOException;
+
 import java.sql.SQLException;
+
+import java.util.LinkedList;
+
 import java.util.logging.Level;
+
 import java.util.logging.Logger;
+
 import javax.ws.rs.Consumes;
+
 import javax.ws.rs.DELETE;
+
 import javax.ws.rs.GET;
+
 import javax.ws.rs.POST;
+
 import javax.ws.rs.PUT;
+
 import javax.ws.rs.Path;
+
 import javax.ws.rs.Produces;
+
 import javax.ws.rs.core.Context;
+
 import javax.ws.rs.core.MediaType;
+
 import javax.ws.rs.core.UriInfo;
+
 import objects.Coffee_Shop;
+
 import org.codehaus.jackson.map.ObjectMapper;
 
-/**
- *
- * @author reid
- */    
-    // TODO: 
-    //  Create a PSOT for json
-    
-    //  Create GET for json
-    
+
 
 /**
- * REST Web Service
+
  *
+
+ * @author reid
+
+ */    
+
+    // TODO: 
+
+    //  Create a PSOT for json
+
+    
+
+    //  Create GET for json
+
+    
+
+
+
+/**
+
+ * REST Web Service
+
+ *
+
  * @author wlloyd
+
  */
+
 @Path("Coffeeshop")
+
 public class Coffee_shopService {
 
+
+
     static final Logger logger = Logger.getLogger(UserService.class.getName());
+
     
+
     @Context
+
     private UriInfo context;
 
+
+
     /**
+
      * Creates a new instance of GenericResource
+
      */
+
     public Coffee_shopService() {
+
     }
 
+
+
     /**
+
      * Retrieves representation of an instance of services.GenericResource
+
      * @return an instance of java.lang.String
+
      */
+
 	@GET
+
     @Produces(MediaType.TEXT_HTML)
+
     public String getCShops() {
+
         //TODO return proper representation object
+
         StringBuilder sb = new StringBuilder();
+
         sb.append("<html><body><style>table, th, td {font-family:Arial,Verdana,sans-serif;font-size:16px;padding: 0px;border-spacing: 0px;}</style><b>Coffee Shop List:</b><br><br><table cellpadding=10 border=1><tr><td>coffee id</td><td>coffee name</td><td>coffee address</td><td>raw review</td><td>phone number</td><td>url</td></tr>");
+
         try
+
         {
+
             Model db = Model.singleton();
-            Coffee_Shop[] cs = db.getCoffeeShops();
-            for (int i=0;i<cs.length;i++)
-                sb.append("<tr><td>" + cs[i].getCid() + "</td><td>" + cs[i].getCoffeeName() + "</td><td>" + cs[i].getCoffeeAddress() + "</td><td>" + cs[i].getRawReview() + "</td><td>" + cs[i].getPhone() + "</td><td>" + cs[i].getUrl() + "</td></tr>");
+
+            LinkedList<Coffee_Shop> cs = db.getCoffeeShops();
+
+            for(int i=0; i<cs.size(); i++) {
+
+                sb.append("<tr><td>" + cs.get(i).getCid() + "</td><td>" + cs.get(i).getCoffeeName() + "</td><td>" + cs.get(i).getCoffeeAddress() + "</td><td>" + cs.get(i).getRawReview() + "</td><td>" + cs.get(i).getPhone() + "</td><td>" + cs.get(i).getUrl() + "</td></tr>");
+
+
+
+            }
+
+            //Coffee_Shop[] cs = db.getCoffeeShops();
+
+//            for (int i=0;i<cs.length;i++)
+
+//                sb.append("<tr><td>" + cs[i].getCid() + "</td><td>" + cs[i].getCoffeeName() + "</td><td>" + cs[i].getCoffeeAddress() + "</td><td>" + cs[i].getRawReview() + "</td><td>" + cs[i].getPhone() + "</td><td>" + cs[i].getUrl() + "</td></tr>");
+
         }
+
         catch (Exception e)
+
         {
+
             sb.append("</table><br>Error getting coffee shops: " + e.toString() + "<br>");
+
         }
+
         sb.append("</table></body></html>");
+
         return sb.toString();
+
     }
 
+
+
     /**
+
      * PUT method for updating or creating an instance of GenericResource
+
      * @param content representation for the resource
+
      */
+
     @PUT
+
     @Produces(MediaType.TEXT_PLAIN)
+
     @Consumes(MediaType.APPLICATION_JSON)
+
     public String updateCoffeeShop(String jobj) throws IOException
+
     {
-        
+
+        /*
+
         ObjectMapper mapper = new ObjectMapper();
-        Coffee_Shop cs = mapper.readValue(jobj.toString(), Coffee_Shop.class);
+
+        User user = mapper.readValue(jobj.toString(), User.class);
+
         StringBuilder text = new StringBuilder();
+
         try {
+
             Model db = Model.singleton();
-            int cid = cs.getCid();
-            db.updateCoffeeShop(cs);
-            logger.log(Level.INFO, "update coffee_shop with cid=" + cid);
-            text.append("cid updated with  cid=" + cid + "\n");
+
+            int userid = user.getUserid();
+
+            db.updateUser(user);
+
+            logger.log(Level.INFO, "update user with userid=" + userid);
+
+            text.append("User id updated with user id=" + userid + "\n");
+
         }
+
         catch (SQLException sqle)
+
         {
-            String errText = "Error updating coffee_shop after db connection made:\n" + sqle.getMessage() + " --- " + sqle.getSQLState() + "\n";
+
+            String errText = "Error updating user after db connection made:\n" + sqle.getMessage() + " --- " + sqle.getSQLState() + "\n";
+
             logger.log(Level.SEVERE, errText);
+
             text.append(errText);
+
         }
+
         catch (Exception e)
+
         {
+
             logger.log(Level.SEVERE, "Error connecting to db.");
+
             text.append("Error connecting to db.");
+
         }
+
         return text.toString();
+
+*/
+
+        return "";
+
     }
+
     
+
     @DELETE
+
     @Produces(MediaType.TEXT_PLAIN)
+
     @Consumes(MediaType.APPLICATION_JSON)
+
     public String deleteUser(String jobj) throws IOException
+
     {
-        
+
+        /*
+
         ObjectMapper mapper = new ObjectMapper();
-        Coffee_Shop cs = mapper.readValue(jobj.toString(), Coffee_Shop.class);
+
+        User user = mapper.readValue(jobj.toString(), User.class);
+
         StringBuilder text = new StringBuilder();
+
         try {
+
             Model db = Model.singleton();
-            int cid = cs.getCid();
-            db.deleteCoffeeShop(cid);
-            logger.log(Level.INFO, "CoffeeShop deleted from db=" + cid);
-            text.append("Cid deleted with id=" + cid);
+
+            int userid = user.getUserid();
+
+            db.deleteUser(userid);
+
+            logger.log(Level.INFO, "user deleted from db=" + userid);
+
+            text.append("User id deleted with id=" + userid);
+
         }
+
         catch (SQLException sqle)
+
         {
+
             String errText = "Error deleteing user after db connection made:\n" + sqle.getMessage() + " --- " + sqle.getSQLState() + "\n";
+
             logger.log(Level.SEVERE, errText);
+
             text.append(errText);
+
         }
+
         catch (Exception e)
+
         {
+
             logger.log(Level.SEVERE, "Error connecting to db.");
+
             text.append("Error connecting to db.");
+
         }
+
         return text.toString();
+
+*/
+
+        return "";
+
     }
+
     
+
    @POST
+
    @Produces(MediaType.TEXT_PLAIN)
+
    @Consumes(MediaType.APPLICATION_JSON)
+
    public String createCShop(String jobj) throws IOException {
+
        ObjectMapper mapper = new ObjectMapper();
+
        Coffee_Shop shop = mapper.readValue(jobj.toString(), Coffee_Shop.class);
+
        
+
        StringBuilder text = new StringBuilder();
+
        text.append("The JSON obj:" + jobj.toString() + "\n");
+
 //         text.append("Hello " + user.getName() + "\n");
+
 //         text.append("You're only " + user.getAge() + " years old.\n");
+
 //         text.append("Messages:\n");
+
 //         for (Object msg : user.getMessages())
+
 //             text.append(msg.toString() + "\n");
+
        
+
        try {
+
            Model db = Model.singleton();
+
+           //int cid = db.newCoffeeShop(shop);
+
            int cid = db.newCoffeeShop(shop);
+
+           db.newCoffeeShop(shop);
+
            logger.log(Level.INFO, "shop persisted to db as cid=" + cid);
+
            text.append("Shop id persisted with id=" + cid);
+
        }
+
        catch (SQLException sqle)
+
        {
+
            String errText = "Error persisting user after db connection made:\n" + sqle.getMessage() + " --- " + sqle.getSQLState() + "\n";
+
            logger.log(Level.SEVERE, errText);
+
            text.append(errText);
+
        }
+
        catch (Exception e)
+
        {
+
            logger.log(Level.SEVERE, "Error connecting to db.");
+
        }
+
        
+
        
+
        return text.toString();
+
    }
+
 }
--- src/main/java/data/Model.java
+++ src/main/java/data/Model.java
@@ -1,227 +1,417 @@
 /*
+
  * To change this license header, choose License Headers in Project Properties.
+
  * To change this template file, choose Tools | Templates
+
  * and open the template in the editor.
+
  */
+
 package data;
 
+
+
 import java.sql.Connection;
+
 import java.sql.DriverManager;
+
 import java.sql.PreparedStatement;
+
 import java.sql.ResultSet;
+
 import java.sql.SQLException;
+
 import java.sql.Statement;
+
 import java.util.LinkedList;
+
 import java.util.logging.Level;
+
 import java.util.logging.Logger;
+
 import objects.User;
+
 import objects.Coffee_Shop;
 
+
+
 /**
+
  *
+
  * @author wlloyd
+
  */
+
 public class Model {
+
     static final Logger logger = Logger.getLogger(Model.class.getName());
+
     private static Model instance;
+
     private Connection conn;
+
+    private LinkedList<Coffee_Shop> csList = new LinkedList<Coffee_Shop>();
+
     
+
     public static Model singleton() throws Exception {
+
         if (instance == null) {
+
             instance = new Model();
+
         }
+
         return instance;
+
     }
+
     ////blabla
+
     Model() throws Exception
+
     {
+
         Class.forName("org.postgresql.Driver");
+
         String dbUrl = System.getenv("JDBC_DATABASE_URL");
+
         if ((dbUrl == null) || (dbUrl.length() < 1))
+
             dbUrl = System.getProperties().getProperty("DBCONN");
+
         logger.log(Level.INFO, "dbUrl=" + dbUrl);  
+
         logger.log(Level.INFO, "attempting db connection");
+
         conn = DriverManager.getConnection(dbUrl);
+
         logger.log(Level.INFO, "db connection ok.");
+
     }
+
     
+
     private Connection getConnection()
+
     {
+
         return conn;
+
     }
+
     
+
     private Statement createStatement() throws SQLException
+
     {
+
         Connection conn = getConnection();
+
         if ((conn != null) && (!conn.isClosed()))
+
         {
+
             logger.log(Level.INFO, "attempting statement create");
+
             Statement st = conn.createStatement();
+
             logger.log(Level.INFO, "statement created");
+
             return st;
+
         }
+
         else
+
         {
+
             // Handle connection failure
+
         }
+
         return null;
+
     }
+
     
+
     private PreparedStatement createPreparedStatement(String sql) throws SQLException
+
     {
+
         Connection conn = getConnection();
+
         if ((conn != null) && (!conn.isClosed()))
+
         {
+
             logger.log(Level.INFO, "attempting statement create");
+
             PreparedStatement pst = conn.prepareStatement(sql);
+
             logger.log(Level.INFO, "prepared statement created");
+
             return pst;
+
         }
+
         else
+
         {
+
             // Handle connection failure
+
         }
+
         return null;
+
     }
+
     
+
     public int newUser(User usr) throws SQLException
+
     {
+
         String sqlInsert="insert into users (name, age) values ('" + usr.getName() + "'" + "," + usr.getAge() + ");";
+
         Statement s = createStatement();
+
         logger.log(Level.INFO, "attempting statement execute");
+
         s.execute(sqlInsert,Statement.RETURN_GENERATED_KEYS);
+
         logger.log(Level.INFO, "statement executed.  atempting get generated keys");
+
         ResultSet rs = s.getGeneratedKeys();
+
         logger.log(Level.INFO, "retrieved keys from statement");
+
         int userid = -1;
+
         while (rs.next())
+
             userid = rs.getInt(3);   // assuming 3rd column is userid
+
         logger.log(Level.INFO, "The new user id=" + userid);
+
         return userid;
+
     }
+
     
+
     public void deleteUser(int userid) throws SQLException
+
     {
+
         String sqlDelete="delete from users where userid=?";
+
         PreparedStatement pst = createPreparedStatement(sqlDelete);
+
         pst.setInt(1, userid);
+
         pst.execute();
+
     }
+
     
+
     public User[] getUsers() throws SQLException
+
     {
+
         LinkedList<User> ll = new LinkedList<User>();
+
         String sqlQuery ="select * from users;";
+
         Statement st = createStatement();
+
         ResultSet rows = st.executeQuery(sqlQuery);
+
         while (rows.next())
+
         {
+
             logger.log(Level.INFO, "Reading row...");
+
             User usr = new User();
+
             usr.setName(rows.getString("name"));
+
             usr.setUserId(rows.getInt("userid"));
+
             usr.setAge(rows.getInt("age"));
+
             logger.log(Level.INFO, "Adding user to list with id=" + usr.getUserid());
+
             ll.add(usr);
+
         }
+
         return ll.toArray(new User[ll.size()]);
+
     }
+
     
+
     public boolean updateUser(User usr) throws SQLException
+
     {
+
         StringBuilder sqlQuery = new StringBuilder();
+
         sqlQuery.append("update users ");
+
         sqlQuery.append("set name='" + usr.getName() + "', ");
+
         sqlQuery.append("age=" + usr.getAge() + " ");
+
         sqlQuery.append("where userid=" + usr.getUserid() + ";");
+
         Statement st = createStatement();
+
         logger.log(Level.INFO, "UPDATE SQL=" + sqlQuery.toString());
+
         return st.execute(sqlQuery.toString());
+
     }
+
     
+
     public int newCoffeeShop(Coffee_Shop cs) throws SQLException
+
     {
-        String sqlInsert="insert into shops (cid, name, address, rawreview, phone, getUrl) values ('" + cs.getCid() 
-                + "'" + "," + cs.getCoffeeName() + "'" + "," + cs.getCoffeeAddress()
-                + "'" + "," + cs.getRawReview() + "'" + "," + cs.getPhone()
-                + "'" + "," + cs.getUrl() +");";
-        Statement s = createStatement();
-        logger.log(Level.INFO, "attempting statement execute");
-        s.execute(sqlInsert,Statement.RETURN_GENERATED_KEYS);
-        logger.log(Level.INFO, "statement executed.  atempting get generated keys");
-        ResultSet rs = s.getGeneratedKeys();
-        logger.log(Level.INFO, "retrieved keys from statement");
-        int shopid = -1;
-        while (rs.next())
-            shopid = rs.getInt(3);   // assuming 3rd column is userid
-        logger.log(Level.INFO, "The new shop id=" + shopid);
-        return shopid;
+
+            csList.add(cs);
+
+//        String sqlInsert="insert into shops (cid, name, address, rawreview, phone, getUrl) values ('" + cs.getCid() 
+
+//                + "'" + "," + cs.getCoffeeName() + "'" + "," + cs.getCoffeeAddress()
+
+//                + "'" + "," + cs.getRawReview() + "'" + "," + cs.getPhone()
+
+//                + "'" + "," + cs.getUrl() +");";
+
+//        Statement s = createStatement();
+
+//        logger.log(Level.INFO, "attempting statement execute");
+
+//        s.execute(sqlInsert,Statement.RETURN_GENERATED_KEYS);
+
+//        logger.log(Level.INFO, "statement executed.  atempting get generated keys");
+
+//        ResultSet rs = s.getGeneratedKeys();
+
+//        logger.log(Level.INFO, "retrieved keys from statement");
+
+//        int shopid = -1;
+
+//        while (rs.next())
+
+//            shopid = rs.getInt(3);   // assuming 3rd column is userid
+
+//        logger.log(Level.INFO, "The new shop id=" + shopid);
+
+        return cs.getCid();
+
     }
+
     
-    public Coffee_Shop[] getCoffeeShops() throws SQLException {
-        LinkedList<Coffee_Shop> ll = new LinkedList<Coffee_Shop>();
-        String sqlQuery ="select * from shops;";
-        Statement st = createStatement();
-        ResultSet rows = st.executeQuery(sqlQuery);
-        while (rows.next())
-        {
-            logger.log(Level.INFO, "Reading row...");
-            Coffee_Shop cs = new Coffee_Shop();
-            cs.setCid(rows.getInt("cid"));
-            cs.setCoffeeName(rows.getString("coffeeName"));
-            cs.setCoffeeAddress(rows.getString("coffeeAddress"));
-            cs.setRawReview(rows.getInt("rawReview"));
-            cs.setPhone(rows.getString("phone"));
-            cs.setUrl(rows.getString("url"));
-            logger.log(Level.INFO, "Adding shop to list with id=" + cs.getCid());
-            ll.add(cs);
-        }
-        return ll.toArray(new Coffee_Shop[ll.size()]);
+
+    public LinkedList<Coffee_Shop> getCoffeeShops() throws SQLException {
+
+          
+
+//        LinkedList<Coffee_Shop> ll = new LinkedList<Coffee_Shop>();
+
+//        String sqlQuery ="select * from shops;";
+
+//        Statement st = createStatement();
+
+//        ResultSet rows = st.executeQuery(sqlQuery);
+
+////        while (rows.next())
+
+//        {
+
+//            logger.log(Level.INFO, "Reading row...");
+
+//            Coffee_Shop cs = new Coffee_Shop();
+
+//            cs.setCid(rows.getInt("cid"));
+
+//            cs.setCoffeeName(rows.getString("coffeeName"));
+
+//            cs.setCoffeeAddress(rows.getString("coffeeAddress"));
+
+//            cs.setRawReview(rows.getInt("rawReview"));
+
+//            cs.setPhone(rows.getString("phone"));
+
+//            cs.setUrl(rows.getString("url"));
+
+//            logger.log(Level.INFO, "Adding shop to list with id=" + cs.getCid());
+
+//            ll.add(cs);
+
+//        }
+
+//        return ll.toArray(new Coffee_Shop[ll.size()]);
+
+            return csList;
+
     }
+
     
-    public Coffee_Shop[] getCS() throws SQLException
-   {
-       LinkedList<Coffee_Shop> ll = new LinkedList<Coffee_Shop>();
-       String sqlQuery ="select * from shops;";
-       Statement st = createStatement();
-       ResultSet rows = st.executeQuery(sqlQuery);
-       while (rows.next())
-       {
-           logger.log(Level.INFO, "Reading row...");
-           Coffee_Shop cs = new Coffee_Shop();
-           cs.setCoffeeName(rows.getString("coffeeName"));
-           cs.setCid(rows.getInt("cid"));
-           cs.setCoffeeAddress(rows.getString("coffeeAddress"));
-           cs.setRawReview(rows.getInt("rawReview"));
-           cs.setPhone(rows.getString("phone"));
-           cs.setUrl(rows.getString("url"));
-           logger.log(Level.INFO, "Adding shop to list with id=" + cs.getCid());
-           ll.add(cs);
-       }
-       return ll.toArray(new Coffee_Shop[ll.size()]);
-   }
+
     
+
+   
+
+    
+
     public boolean updateCoffeeShop(Coffee_Shop cs) throws SQLException {
-        StringBuilder sqlQuery = new StringBuilder();
-        sqlQuery.append("update shops ");
-        sqlQuery.append("cid='" + cs.getCid() + "', ");
-        sqlQuery.append("coffeeName=" + cs.getCoffeeName() + " ");
-        sqlQuery.append("coffeeAddress=" + cs.getCoffeeAddress() + ",");
-        sqlQuery.append("rawReview=" + cs.getRawReview() + ",");
-        sqlQuery.append("phone=" + cs.getPhone() + ",");
-        sqlQuery.append("url=" + cs.getUrl() + ";");
-        Statement st = createStatement();
-        logger.log(Level.INFO, "UPDATE SQL=" + sqlQuery.toString());
-        return st.execute(sqlQuery.toString());
+
+            csList.peek().setCoffeeAddress("uwt drive");
+
+            
+
+//        StringBuilder sqlQuery = new StringBuilder();
+
+//        sqlQuery.append("update shops ");
+
+//        sqlQuery.append("cid='" + cs.getCid() + "', ");
+
+//        sqlQuery.append("coffeeName=" + cs.getCoffeeName() + " ");
+
+//        sqlQuery.append("coffeeAddress=" + cs.getCoffeeAddress() + ",");
+
+//        sqlQuery.append("rawReview=" + cs.getRawReview() + ",");
+
+//        sqlQuery.append("phone=" + cs.getPhone() + ",");
+
+//        sqlQuery.append("url=" + cs.getUrl() + ";");
+
+//        Statement st = createStatement();
+
+//        logger.log(Level.INFO, "UPDATE SQL=" + sqlQuery.toString());
+
+            
+
+        return false;
+
         
+
     }
+
     
+
     
-    public void deleteCoffeeShop(int cid) throws SQLException
-   {
-       String sqlDelete="delete from shops where cid=" + cid;
-       PreparedStatement pst = createPreparedStatement(sqlDelete);
-       pst.setInt(1, cid);
-       pst.execute();
-   }
+
+    
+
 }
