cb27d157a0a16914fe0e08e2e5a9d7e4d16071be
tim weaver <timaw85@gmail.com>
2017-02-28 18:49:31 -0800

    fixed endpoint

--- src/scripts/delete_shop.sh
+++ src/scripts/delete_shop.sh
@@ -2,7 +2,7 @@ ENDPOINT=https://team4360.herokuapp.com/tcss360/Coffeeshop
 # heroku
 #ENDPOINT=https://frozen-temple-15527.herokuapp.com/tcss360/users
 # local web runner deploy
-ENDPOINT=http://localhost:8080/tcss360/users
+#ENDPOINT=http://localhost:8080/tcss360/users
 # net beans deploy
 #ENDPOINT=http://localhost:8084/sample_maven_web_app/tcss360/users
 # manual deploy
