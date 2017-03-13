e15fd0d8a2a70876bdb356150dc568a249843871
cupins <cupins@users.noreply.github.com>
2017-03-12 18:47:49 -0700

    comments and added whitespace

--- src/scripts/z.sh
+++ src/scripts/z.sh
@@ -1,10 +1,12 @@
 echo "Hello World i might suck"
-hope=https://rickrolls.herokuapp.com/tcss360/shops
+hope=https://team4360.herokuapp.com/tcss360/shops
+
+#ENDPOINT SHOULD BE HERE CHANGE TO DESIRED
 ENDPOINT=https://gentle-coast-59786.herokuapp.com/tcss360/coffeeShop/api/shops
 chris=$(curl -X GET -H "Content-Type: application/html" $ENDPOINT)
-echo $chris
+#echo $chris
 count=$(python helper.py $chris)
-echo $count
+#echo $count
 for (( c=1; c<=$count; c++ ))
 do
     json=$(python jp.py $chris $c)
@@ -12,5 +14,5 @@ do
     echo "we json"
     curl -X POST -H "Content-Type: application/json" --data $json $hope
 done
-echo "we won"
+#echo "we won"
 
