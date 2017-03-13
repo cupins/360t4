2d6aafb72823fb609250d18992061995d7cabf3c
cupins <thestuberg@gmail.com>
2017-03-12 18:53:37 -0700

    scripts

--- src/scripts/update_shop.sh
+++ src/scripts/update_shop.sh
@@ -1,5 +1,4 @@
-ENDPOINT=https://rickrolls.herokuapp.com/tcss360/shops
-# local web runner deploy
+ENDPOINT=https://team4360.herokuapp.com/tcss360/shops
 #ENDPOINT=http://localhost:8080/tcss360/users
 # net beans deploy
 #ENDPOINT=http://localhost:8084/sample_maven_web_app/tcss360/users
