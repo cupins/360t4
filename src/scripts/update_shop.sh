1f2c3a8fb6da476a00ecd8895b41ff0ad2e27ce4
cupins <cupins@users.noreply.github.com>
2017-03-12 18:58:40 -0700

    Update update_shop.sh

--- src/scripts/update_shop.sh
+++ src/scripts/update_shop.sh
@@ -4,6 +4,6 @@ ENDPOINT=https://team4360.herokuapp.com/tcss360/shops
 #ENDPOINT=http://localhost:8084/sample_maven_web_app/tcss360/users
 # manual deploy
 #ENDPOINT=http://localhost:8080/sample_maven_web_app-1.0-SNAPSHOT/tcss360/users
-curl -X PUT -H "Content-Type: application/json" -d @./shop_five_update.json $ENDPOINT
+curl -X PUT -H "Content-Type: application/json" -d @./shop_one_update.json $ENDPOINT
 echo
 
